9198b9b0e73c464c41494d8a58dd9c8339188792
kevin99872 <benny20051211@gmail.com>
2026-03-24 15:29:53 +0800

    update:add more note

---
+++ W5/W5_1.py
@@ -0,0 +1,75 @@
+import torch
+import torch.nn as nn
+
+class ResNetBlock(nn.Module): #定義一個殘差塊類別
+
+    def __init__(self, dim):
+        super(ResNetBlock, self).__init__()
+        self.conv_block = self.build_conv_block(dim)
+
+    def build_conv_block(self, dim):
+        conv_block = []
+
+        conv_block += [nn.ReflectionPad2d(1)] #對輸入圖片進行填補操作
+
+        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=0, bias=True), #進行卷積處理
+                       nn.InstanceNorm2d(dim),
+                       nn.ReLU(True)]
+
+        conv_block += [nn.ReflectionPad2d(1)]
+
+        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=0, bias=True),
+                       nn.InstanceNorm2d(dim)]
+
+        return nn.Sequential(*conv_block) 
+
+    def forward(self, x):
+        out = x + self.conv_block(x)
+        return out
+
+
+class ResNetGenerator(nn.Module): #定義一個生成器網路
+
+    def __init__(self, input_nc=3, output_nc=3, ngf=64, n_blocks=9): 
+
+        assert(n_blocks >= 0)
+        super(ResNetGenerator, self).__init__()
+
+        self.input_nc = input_nc
+        self.output_nc = output_nc
+        self.ngf = ngf
+
+        model = [nn.ReflectionPad2d(3),
+                 nn.Conv2d(input_nc, ngf, kernel_size=7, padding=0, bias=True),
+                 nn.InstanceNorm2d(ngf),
+                 nn.ReLU(True)]
+
+        n_downsampling = 2
+        for i in range(n_downsampling): #進行降採樣處理
+            mult = 2**i
+            model += [nn.Conv2d(ngf * mult, ngf * mult * 2, kernel_size=3,
+                                stride=2, padding=1, bias=True),
+                      nn.InstanceNorm2d(ngf * mult * 2),
+                      nn.ReLU(True)]
+
+        mult = 2**n_downsampling
+        for i in range(n_blocks): #組合多個殘差塊
+            model += [ResNetBlock(ngf * mult)]
+
+        for i in range(n_downsampling):
+            mult = 2**(n_downsampling - i)
+            model += [nn.ConvTranspose2d(ngf * mult, int(ngf * mult / 2),
+                                         kernel_size=3, stride=2,
+                                         padding=1, output_padding=1,
+                                         bias=True),
+                      nn.InstanceNorm2d(int(ngf * mult / 2)),
+                      nn.ReLU(True)]
+
+        model += [nn.ReflectionPad2d(3)]
+        model += [nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0)]
+        model += [nn.Tanh()]
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, input): 
+        return self.model(input)
